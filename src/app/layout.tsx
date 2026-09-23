9a6ce7919a18c9be687e7b96185c069e51767929
FairyNguyen3012 <nguyenngocnu3012@gmail.com>
2026-09-23 15:44:37 +0700

    fix: suppress extension body hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <SiteHeader />
         <main className="mx-auto w-full max-w-5xl flex-1 px-4 py-6 sm:px-6 sm:py-10">
           {children}
